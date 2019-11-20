55c640a09fa4586f3ba124a6a33f13510db4e084
Serafettin Bayram <sbayram2016@gmail.com>
2019-11-20 14:26:26 +0100

    exercise1 modified

--- week1/homework/nodejs-exercises/exercise1/app.js
+++ week1/homework/nodejs-exercises/exercise1/app.js
@@ -4,6 +4,6 @@ const andrejsFunction = require('./andrejs-awesome-function.js');
 const numbers = ['12', '846', '2', '1236'];
 
 numbers.forEach(number => {
-  const requestedNumbers = andrejsFunction.padLeft(number, 5, '');
+  const requestedNumbers = andrejsFunction.padLeft(number, 4, ' ');
   console.log(requestedNumbers);
 });
